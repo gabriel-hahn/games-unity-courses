11f8c08ab9c399f2801e246898d2aa8c46b96dec
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-05-31 10:42:46 -0300

    Initial password validation - Checking if it is correct

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# games-unity-courses
+# Games Unity Courses - C#
+
 Repository with all games developed during courses and using Unity as game engine :space_invader: :video_game:
--- TerminalHacker/Assets/Hacker.cs
+++ TerminalHacker/Assets/Hacker.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum Screen
+{
+    MainMenu,
+    Password,
+    Win
+};
+
 public class Hacker : MonoBehaviour
 {
-
     int level;
+    string password;
+    Screen currentScreen = Screen.MainMenu;
 
     void Start()
     {
@@ -14,6 +22,7 @@ public class Hacker : MonoBehaviour
 
     void ShowMainMenu ()
     {
+        currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
 
         Terminal.WriteLine("What would you like to hack into?");
@@ -28,14 +37,28 @@ public class Hacker : MonoBehaviour
         {
             ShowMainMenu();
         }
-        else if (input == "1")
+        else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(input);
+        }
+        else if (currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
+        }
+    }
+
+    void RunMainMenu(string input)
+    {
+        if (input == "1")
         {
             level = 1;
+            password = "monkey";
             StartGame();
         }
         else if (input == "2")
         {
             level = 2;
+            password = "environment";
             StartGame();
         }
         else if (input == "007")
@@ -48,8 +71,22 @@ public class Hacker : MonoBehaviour
         }
     }
 
+    void CheckPassword(string input)
+    {
+        if (input == password)
+        {
+            Terminal.WriteLine("Well done!");
+        }
+        else
+        {
+            Terminal.WriteLine("Sorry, wrong password!");
+        }
+    }
+
     void StartGame()
     {
+        currentScreen = Screen.Password;
         Terminal.WriteLine("You have chosen level " + level);
+        Terminal.WriteLine("Please enter your password: ");
     }
 }
