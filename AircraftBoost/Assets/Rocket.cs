829df55efebfc129642d4b50296de3bc5d53cdb8
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-06-01 22:13:16 -0300

    Initial rocket script and mass adjustments

---
+++ AircraftBoost/Assets/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rigiBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigiBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigiBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigiBody.AddRelativeForce(Vector3.right);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rigiBody.AddRelativeForce(Vector3.left);
+        }
+    }
+}
