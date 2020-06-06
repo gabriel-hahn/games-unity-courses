6d5f37d041c4271640d9f4f56d3dfdbea6c9f8d8
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-06-06 19:08:39 -0300

    Animation fixed and create initial movement script

---
+++ TopDownShooter/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    private new Rigidbody2D rigidbody;
+    private Vector2 moveAmount;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveAmount = moveInput.normalized * speed;
+
+        animator.SetBool("isRunning", moveInput != Vector2.zero);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + moveAmount * Time.fixedDeltaTime);
+    }
+}
