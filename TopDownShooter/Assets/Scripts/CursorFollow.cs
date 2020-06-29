f7518d1259f4c5a75b7c9f856b97cc87b177669e
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-06-29 18:42:26 -0300

    Target cursor added

---
+++ TopDownShooter/Assets/Scripts/CursorFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CursorFollow : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        transform.position = Input.mousePosition;
+    }
+}
